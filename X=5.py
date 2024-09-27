cc125466702db58930a76d7d3ae0850b8d086361
Surendra Cheekurthi <cheekurthisurendra743@gmail.com>
2024-09-27 15:53:38 +0530

    first

---
+++ X=5.py
@@ -0,0 +1,41 @@
+=5
+print(x)
+name="surendra"
+print(name)
+y=10
+print(y)
+z=x+y
+print(z)
+if x  < y:
+    print(x)
+
+    fruits=["apple","banana","orange"]
+print(fruits)
+i = 0
+while i > 5:
+    print(i)
+    i += 1
+    fruits = ["apple", "banana", "cherry"]
+print(fruits[2])  
+
+fruits = ["apple", "banana", "cherry"]
+print(fruits[1:3])  
+fruits.append("orange")
+print(fruits)  
+
+name="20.8"
+print(name)
+
+
+s={24,43,34,24,56,88,34,1,0,3}
+print(s)
+s.add(900)
+print(s)
+s.update({22,44,55,66,77,88,99})
+print(s)
+s.pop()
+print(s)
+
+v2=({11,22,33,44,55})
+v3=({99,88,77,66})
+print(v2.union(v2))
